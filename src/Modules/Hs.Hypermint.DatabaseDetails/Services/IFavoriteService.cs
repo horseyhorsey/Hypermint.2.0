e1e15595ee32f6eb53107b216de83a912e8c9891
dave <dub.cyclone@gmail.com>
2016-02-11 20:10:41 +0000

    Added Favorite service to pull in favorites from text. resolves #48

---
+++ src/Modules/Hs.Hypermint.DatabaseDetails/Services/IFavoriteService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Hs.Hypermint.DatabaseDetails.Services
+{
+    public interface IFavoriteService
+    {
+        List<string> GetFavoritesForSystem (string system, string hyperSpinPath);
+    }
+}
