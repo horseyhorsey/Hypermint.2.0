ea8049a3b9ee0149865b5c6af05c416e5cf73e3a
dave <dub.cyclone@gmail.com>
2016-02-12 23:53:56 +0000

    Added: null checking system names before updating favorites

--- src/Modules/Hs.Hypermint.DatabaseDetails/Services/FavoriteService.cs
+++ src/Modules/Hs.Hypermint.DatabaseDetails/Services/FavoriteService.cs
@@ -9,24 +9,37 @@ namespace Hs.Hypermint.DatabaseDetails.Services
     {
         public List<string> GetFavoritesForSystem(string system, string hyperSpinPath)
         {
-            var favoriteTextFile = Path.Combine(hyperSpinPath,
-                Root.Databases,
-                system,
-                "favorites.txt"
-                );
-
             var favoritesList = new List<string>();
 
-            if (!File.Exists(favoriteTextFile))
-                return favoritesList;
-
-            using (StreamReader reader = new StreamReader(favoriteTextFile))
+            if (!string.IsNullOrEmpty(system))
             {
-                var line = string.Empty;
-                while ((line = reader.ReadLine()) != null)
+                try
                 {
-                    favoritesList.Add(line);
+                    var favoriteTextFile = Path.Combine(hyperSpinPath,
+                   Root.Databases,
+                   system,
+                   "favorites.txt"
+                   );
+
+                    if (!File.Exists(favoriteTextFile))
+                        return favoritesList;
+
+                    using (StreamReader reader = new StreamReader(favoriteTextFile))
+                    {
+                        var line = string.Empty;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            favoritesList.Add(line);
+                        }
+                    }
                 }
+                catch (Exception)
+                {
+
+                    
+                }
+               
+
             }
 
             return favoritesList;
