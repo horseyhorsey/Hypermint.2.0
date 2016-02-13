60de10a38eb4d410509a94231f97dd298cf208cb
dave <dub.cyclone@gmail.com>
2016-02-13 13:44:06 +0000

    Include Filefolderservice

---
+++ StandardTests/FileFolderServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hs.Hypermint.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hs.Hypermint.Services.Tests
+{
+    [TestClass()]
+    public class FileFolderServiceTests
+    {
+        [TestMethod()]
+        public void OpenFolderTest()
+        {
+            var f = new FileFolderService();
+
+            f.OpenFolder(@"C:\");
+        }
+    }
+}
