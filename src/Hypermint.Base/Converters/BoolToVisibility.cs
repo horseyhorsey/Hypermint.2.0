e1f25aa8bf005136e2aa0710e8e81e90aef25d26
dave <dub.cyclone@gmail.com>
2016-02-13 11:36:17 +0000

    Added: Bool to visibilty converter

---
+++ src/Hypermint.Base/Converters/BoolToVisibility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Hypermint.Base.Converters
+{
+    public class BoolToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visible = System.Windows.Visibility.Visible;
+
+            try
+            {
+                var parseBool =
+                    bool.Parse(value.ToString());
+                if (parseBool)
+                    visible = System.Windows.Visibility.Collapsed;
+                else
+                    visible = System.Windows.Visibility.Visible;
+            }
+            catch (Exception) { }
+
+            return visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
