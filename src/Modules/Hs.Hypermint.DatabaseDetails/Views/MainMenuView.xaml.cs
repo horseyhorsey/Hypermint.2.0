391d2a885e1dba6cf73e232e8bd08f9003969490
dave <dub.cyclone@gmail.com>
2016-03-21 01:34:25 +0000

    + Basic main menu view in place of normal database view. User can edit from the systems list and doesn't need to see this database view.

---
+++ src/Modules/Hs.Hypermint.DatabaseDetails/Views/MainMenuView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hs.Hypermint.DatabaseDetails.Views
+{
+    /// <summary>
+    /// Interaction logic for MainMenuView
+    /// </summary>
+    public partial class MainMenuView : UserControl
+    {
+        public MainMenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
