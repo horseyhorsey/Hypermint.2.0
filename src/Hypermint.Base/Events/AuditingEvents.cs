b380c15c49d75249ad56734dc563952d5516b500
dave <dub.cyclone@gmail.com>
2016-03-22 09:12:28 +0000

    + new auditing events

---
+++ src/Hypermint.Base/Events/AuditingEvents.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Events;
+
+namespace Hypermint.Base.Events
+{
+    public class AuditHyperSpinEvent : PubSubEvent<string> { }
+
+    public class AuditMediaTypeEvent : PubSubEvent<string> { }
+
+    public class AuditHyperSpinEndEvent : PubSubEvent<string> { }
+    
+}
