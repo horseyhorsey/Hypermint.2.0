cc36b86b7f2fd2a5ec4e8d5fd223dd6cadc85940
dave <dub.cyclone@gmail.com>
2016-03-22 16:21:56 +0000

    + Searching for games for multisystems on scan

--- src/Modules/Hs.Hypermint.DatabaseDetails/Services/HyperspinXmlService.cs
+++ src/Modules/Hs.Hypermint.DatabaseDetails/Services/HyperspinXmlService.cs
@@ -4,6 +4,9 @@ using System.IO;
 using Hypermint.Base.Constants;
 using System.Collections.Generic;
 using System;
+using System.Xml.Linq;
+using System.Xml;
+using System.Linq;
 
 namespace Hs.Hypermint.DatabaseDetails.Services
 {
@@ -13,10 +16,10 @@ namespace Hs.Hypermint.DatabaseDetails.Services
         {
             var genreNames = GetGenreNames(ref gamesList);
 
-            SerializeGenreMainMenu(genreNames, hyperspinPath,systemName);
-            
-            foreach (var dbName in genreNames)
-            {
+            SerializeGenreMainMenu(genreNames, hyperspinPath, systemName);
+
+            foreach (string dbName in genreNames)
+            {                
                 var tempGamesBuilder = new Games();
 
                 if (dbName == systemName) return false;
@@ -30,7 +33,7 @@ namespace Hs.Hypermint.DatabaseDetails.Services
                     }
 
                     SerializeHyperspinXml(tempGamesBuilder, systemName, hyperspinPath, dbName);
-                }                
+                }
             }
 
             return true;
@@ -82,9 +85,12 @@ namespace Hs.Hypermint.DatabaseDetails.Services
 
             foreach (var game in gamesList)
             {
-            
+
                 if (!genreNames.Contains(game.Genre))
+                {
+                    game.Genre = game.Genre.Replace("/", " ").Replace(@"\", " ");
                     genreNames.Add(game.Genre);
+                }
             }
 
             genreNames.Sort();
@@ -92,18 +98,24 @@ namespace Hs.Hypermint.DatabaseDetails.Services
             return genreNames;
         }
 
-        public bool SerializeHyperspinXml(Games gamesList, string systemName, 
-            string hyperspinPath, string dbName = "")
-        {            
+        public bool SerializeHyperspinXml(Games gamesList, string systemName,
+            string hyperspinPath, string dbName = "",bool isMultiSystem = false)
+        {
             if (dbName != "Favorites")
             {
                 if (string.IsNullOrEmpty(dbName))
                     dbName = systemName;
             }
-            
-            var dbXmlFileName = dbName + ".xml";
+
+            var dbXmlFileName = dbName + ".xml";            
+
             string finalPath = Path.Combine(hyperspinPath, Root.Databases, systemName, dbXmlFileName);
             var databasePath = Path.Combine(hyperspinPath, Root.Databases, systemName);
+
+            if (isMultiSystem)
+            {
+                File.Create(databasePath + "\\_multisystem");
+            }
 
             if (!File.Exists(finalPath))
             {
@@ -113,9 +125,13 @@ namespace Hs.Hypermint.DatabaseDetails.Services
                 {
                     file.Close();
                 }
-                
+
             }
-                
+
+            var games = new List<Game>(gamesList);
+
+            games.Sort();            
+
             TextWriter textWriter = new StreamWriter(finalPath);
 
             if (!Directory.Exists(databasePath))
@@ -124,14 +140,19 @@ namespace Hs.Hypermint.DatabaseDetails.Services
             XmlSerializer serializer;
 
             var xmlNameSpace = new XmlSerializerNamespaces();
-            xmlNameSpace.Add("", "");            
+            xmlNameSpace.Add("", "");
 
             var xmlRootAttr = new XmlRootAttribute("menu");
-            
+
             if (!systemName.Contains("Main Menu"))
-            {                
-                serializer = new XmlSerializer(typeof(Games), xmlRootAttr);
-                serializer.Serialize(textWriter, gamesList, xmlNameSpace);
+            {
+                try
+                {
+                    serializer = new XmlSerializer(typeof(List<Game>), xmlRootAttr);
+                    serializer.Serialize(textWriter, games, xmlNameSpace);
+                }
+                catch (Exception e) { }
+
             }
             else
             {
@@ -145,7 +166,7 @@ namespace Hs.Hypermint.DatabaseDetails.Services
                 serializer = new XmlSerializer(typeof(List<MainMenu>), xmlRootAttr);
                 serializer.Serialize(textWriter, menuItems, xmlNameSpace);
             }
-                                                                        
+
             textWriter.Close();
 
             return true;
@@ -153,7 +174,7 @@ namespace Hs.Hypermint.DatabaseDetails.Services
         }
 
         public bool SerializeMainMenuXml(Systems systemList, string hyperspinPath, string mainMenuName = "Main Menu")
-        {            
+        {
             var finalPath = Path.Combine(hyperspinPath, Root.Databases, "Main Menu", mainMenuName + ".xml");
             var databasePath = Path.Combine(hyperspinPath, Root.Databases, "Main Menu");
             TextWriter textWriter = new StreamWriter(finalPath);
@@ -183,7 +204,7 @@ namespace Hs.Hypermint.DatabaseDetails.Services
             {
                 if (!item.Name.Contains("Main Menu"))
                     menuItems.Add(new MainMenu(item.Name, item.Enabled));
-            }            
+            }
 
             try
             {
@@ -195,6 +216,149 @@ namespace Hs.Hypermint.DatabaseDetails.Services
             textWriter.Close();
 
             return true;
+        }
+
+
+        public Game SearchGameFromXml(string romName, string system, string hyperspinPath)
+        {
+            var fetchedGame = new Game();
+
+            string mainMenuXml = Path.Combine(hyperspinPath, Root.Databases, system, system + ".xml");
+
+            XDocument xdoc = null;
+
+            using (var xmlreader = XmlReader.Create(mainMenuXml))
+            {
+                try { xdoc = XDocument.Load(xmlreader); }
+                catch (XmlException e) { return fetchedGame; }
+            }
+
+            try
+            {
+                var gameVars =
+                from item in xdoc.Descendants("game")
+                where (item.Attribute("name").Value.Contains(romName) && (item.Element("cloneof").Value == ""))
+
+                select new
+                {
+                    RomName = item.Attribute("name").Value,
+                    Enabled = 1,
+                    Description = item.Element("description").Value,
+                    CloneOf = item.Element("cloneof").Value,
+                    CRC = item.Element("crc").Value,
+                    Manufacturer = item.Element("manufacturer").Value,
+                    Genre = item.Element("genre").Value,
+                    Year = Convert.ToInt32(item.Element("year").Value),
+                    Rating = item.Element("rating").Value,
+                    System = system
+                };
+
+                try
+                {
+                    foreach (var game in gameVars)
+                    {
+                        if (game != null)
+                        {
+                            fetchedGame = new Game()
+                            {
+                                RomName = game.RomName,
+                                Description = game.Description,
+                                CloneOf = game.CloneOf,
+                                Crc = game.CRC,
+                                Manufacturer = game.Manufacturer,
+                                Year = game.Year,
+                                Genre = game.Genre,
+                                Rating = game.Rating,
+                                Enabled = game.Enabled,
+                                System = game.System,
+                                IsFavorite = true
+                            };
+                        }
+                    }
+                }
+                catch (NullReferenceException e) { }
+            }
+            catch { }            
+            
+            return fetchedGame;
+        }
+
+        public List<Game> SearchRomStringsListFromXml(List<string> romsNamesList, string system, string hyperspinPath)
+        {
+            var fetchedGames = new List<Game>();
+
+            string mainMenuXml = Path.Combine(hyperspinPath, Root.Databases, system, system + ".xml");
+
+            XDocument xdoc = null;
+
+            using (var xmlreader = XmlReader.Create(mainMenuXml))
+            {
+                try { xdoc = XDocument.Load(xmlreader); }
+                catch (XmlException e) { return fetchedGames; }
+            }
+
+            try
+            {
+                foreach (string romName in romsNamesList)
+                {
+                    var gameVars =
+               from item in xdoc.Descendants("game")
+               where (item.Attribute("name").Value.Contains(romName) && (item.Element("cloneof").Value == ""))
+
+               select new
+               {
+                   RomName = item.Attribute("name").Value,
+                   Enabled = 1,
+                   Description = item.Element("description").Value,
+                   CloneOf = item.Element("cloneof").Value,
+                   CRC = item.Element("crc").Value,
+                   Manufacturer = item.Element("manufacturer").Value,
+                   Genre = item.Element("genre").Value,
+                   Year = Convert.ToInt32(item.Element("year").Value),
+                   Rating = item.Element("rating").Value,
+                   System = system
+               };
+                    
+                    try
+                    {
+                        Game fetchedGame = new Game();
+
+                        foreach (var game in gameVars)
+                        {
+
+                            if (game != null && !string.IsNullOrEmpty(game.RomName))
+                            {
+                                fetchedGame = new Game()
+                                {
+                                    RomName = game.RomName,
+                                    Description = game.Description,
+                                    CloneOf = game.CloneOf,
+                                    Crc = game.CRC,
+                                    Manufacturer = game.Manufacturer,
+                                    Year = game.Year,
+                                    Genre = game.Genre,
+                                    Rating = game.Rating,
+                                    Enabled = game.Enabled,
+                                    System = game.System,
+                                    IsFavorite = true
+                                };
+                            }
+
+                            if (string.IsNullOrEmpty(game.Description))
+                                fetchedGame.Description = game.RomName;
+
+                            fetchedGames.Add(fetchedGame);
+                            
+                        }
+
+                    }
+                    catch (NullReferenceException e) { }                
+            catch { }
+        }
+            }
+            catch { }
+
+            return fetchedGames;
         }
     }
 }
--- src/Modules/Hs.Hypermint.DatabaseDetails/Services/IHyperspinXmlService.cs
+++ src/Modules/Hs.Hypermint.DatabaseDetails/Services/IHyperspinXmlService.cs
@@ -6,12 +6,16 @@ namespace Hs.Hypermint.DatabaseDetails.Services
     public interface IHyperspinXmlService
     {
         bool SerializeHyperspinXml(Games gamesList, string systemName,
-            string hyperspinPath, string dbName = "");
+            string hyperspinPath, string dbName = "", bool isMultiSystem = false);
 
         bool SerializeGenreXml(Games gamesList, string systemName, string hyperspinPath);
 
         bool SerializeMainMenuXml(Systems systemList, string hyperspinPath,
             string mainMenuName = "Main Menu");
+
+        Game SearchGameFromXml(string romName, string system, string hyperspinPath);
+
+        List<Game> SearchRomStringsListFromXml(List<string> romsNamesList, string system, string hyperspinPath);
 
     }
 }
