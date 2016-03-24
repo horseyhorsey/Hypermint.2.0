f9067952a448d50d58eab6b232047a36f4bd6cde
dave <dub.cyclone@gmail.com>
2016-03-24 07:13:38 +0000

    Refactored and create new database picker. + save database dialogs and moved all controls to a custom dialog window.

---
+++ src/Modules/Hs.Hypermint.DatabaseDetails/Views/DatabasePickerView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hs.Hypermint.DatabaseDetails.Views
+{
+    /// <summary>
+    /// Interaction logic for DatabasePickerView
+    /// </summary>
+    public partial class DatabasePickerView : UserControl
+    {
+        public DatabasePickerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
