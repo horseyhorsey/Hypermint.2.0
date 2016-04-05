8279b816c48bde2f20d420a687c66e61e6516ccb
dave <dub.cyclone@gmail.com>
2016-04-05 14:14:42 +0100

    + AviSynthOption class for script settings.

---
+++ src/Hypermint.Base/Models/AviSynthOption.cs
@@ -0,0 +1,17 @@
+﻿namespace Hypermint.Base.Models
+{
+    public class AviSynthOption
+    {
+        public bool Overlay { get; set; }
+        public int StartFrame { get; set; }
+        public int EndFrame { get; set; }
+        public int DissolveAmount { get; set; }
+        public int FadeIn { get; set; }
+        public int FadeOut { get; set; }
+        public int ResizeWidth { get; set; }
+        public int ResizeHeight { get; set; }
+        public int WheelPosX { get; set; }
+        public int WheelPosY { get; set; }
+
+    }
+}
