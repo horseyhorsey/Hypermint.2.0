9c687f149993714d3edea8d650885998031d4117
dave <dub.cyclone@gmail.com>
2016-05-14 20:50:14 +0100

    add xml searchtests.

---
+++ src/Modules/Hs.Hypermint.DatabaseDetailsSearchTests/Services/HyperspinXmlServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Hs.Hypermint.DatabaseDetails.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hs.Hypermint.DatabaseDetails.Services.Tests
+{
+    [TestFixture()]
+    public class HyperspinXmlServiceTests
+    {
+        [Test()]
+        public void SearchGamesTest()
+        {
+            IHyperspinXmlService hs = new HyperspinXmlService();
+
+            var hsPath = @"I:\Hyperspin";
+
+            var games = hs.SearchGames(hsPath, "Amstrad CPC", "Dizzy");
+
+            Assert.IsNotNull(games);
+        }
+    }
+}
