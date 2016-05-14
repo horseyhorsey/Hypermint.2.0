0232e51918c8233e92122dccc9accf92c59a3709
dave <dub.cyclone@gmail.com>
2016-05-14 17:14:03 +0100

    Added webbrowserModule

---
+++ src/Modules/Hs.Hypermint.Browser/ViewModels/WebBrowseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Hypermint.Base.Base;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hs.Hypermint.Browser.ViewModels
+{
+    public class WebBrowseViewModel : ViewModelBase
+    {
+        public WebBrowseViewModel()
+        {
+
+        }
+    }
+}
