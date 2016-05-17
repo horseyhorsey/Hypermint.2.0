a2bb5aac463b47339fd6c196ef6b7a82df523847
dave <dub.cyclone@gmail.com>
2016-05-17 01:29:14 +0100

    +pdf service interface

---
+++ src/Hypermint.Base/Interfaces/IPdfService.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace Hypermint.Base.Interfaces
+{
+    public interface IPdfService
+    {
+        int GetNumberOfPdfPages(string pdfFile);
+
+        ImageSource GetPage(string ghostScriptPath, string pdfFile, int pageNumber);
+    }
+}
