0b1a20857cc2e05bcd17f085c171b7be994afe03
dave <dub.cyclone@gmail.com>
2016-05-22 23:05:14 +0100

    + WheelTextSetting model class, to load & save presets

---
+++ src/Modules/Hs.Hypermint.WheelCreator/Models/WheelTextSetting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Media;
+using System.Xml.Serialization;
+
+namespace Hs.Hypermint.WheelCreator.Models
+{
+    [Serializable]
+    [XmlRoot(ElementName = "Preset")]
+    public class WheelTextSetting
+    {        
+        public string Name { get; set; }
+        public string FontName { get; set; }
+        public string PreviewText { get; set; }
+        public Color TextColor { get; set; }
+        public Color TextStrokeColor { get; set; }
+        public Color ShadowColor { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool Trim { get; set; }
+        public string Gravity { get; set; }
+        public double ArcAmount { get; set; }
+        public double StrokeWidth { get; set; }
+        public int ShadowX { get; set; }
+        public int ShadowY { get; set; }
+        public int ShadowPercentage { get; set; }
+        public double ShadowSigma { get; set; }
+        public bool Swap { get; set; }
+        public bool Repage { get; set; }
+        public bool Shade { get; set; }
+        public bool ShadeOn { get; set; }
+        public bool ColorShade { get; set; }        
+        public double ShadeAzimuth { get; set; }
+        public double ShadeElevation { get; set; }
+
+    }
+}
