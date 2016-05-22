e836424da6ae9fb72cfd11cc1f3168f9835493c4
dave <dub.cyclone@gmail.com>
2016-05-22 23:03:24 +0100

    + color converter

---
+++ src/Modules/Hs.Hypermint.WheelCreator/Converters/ColorConvert.cs
@@ -0,0 +1,8 @@
+﻿namespace Hs.Hypermint.WheelCreator.Converters
+{
+    public class ColorConvert
+    {
+        public static System.Drawing.Color ColorFromMediaColor(System.Windows.Media.Color clr)
+            => System.Drawing.Color.FromArgb(clr.A, clr.R, clr.G, clr.B);
+    }
+}
