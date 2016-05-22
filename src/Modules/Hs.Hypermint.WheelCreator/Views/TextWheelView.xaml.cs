f27ca0b80159e14a9227a0d4bc4ddbc1b607b884
dave <dub.cyclone@gmail.com>
2016-05-22 23:09:52 +0100

    Refactor

---
+++ src/Modules/Hs.Hypermint.WheelCreator/Views/TextWheelView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hs.Hypermint.WheelCreator.Views
+{
+    /// <summary>
+    /// Interaction logic for TextWheelView
+    /// </summary>
+    public partial class TextWheelView : UserControl
+    {
+        public TextWheelView()
+        {
+            InitializeComponent();
+        }
+    }
+}
