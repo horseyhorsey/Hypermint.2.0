97ce3629539587cb25c69477c234c69dcb08ea17
dave <dub.cyclone@gmail.com>
2016-05-22 23:05:50 +0100

    +TextImage Repo & services

---
+++ src/Modules/Hs.Hypermint.WheelCreator/Services/ITextImageService.cs
@@ -0,0 +1,15 @@
+﻿using Hs.Hypermint.WheelCreator.Models;
+using ImageMagick;
+using System.Threading.Tasks;
+
+namespace Hs.Hypermint.WheelCreator.Services
+{
+    public interface ITextImageService
+    {
+        Task<MagickImage> GenerateCaptionAsync(WheelTextSetting setting);
+
+        string[] GetTextPresets();
+
+        WheelTextSetting DeserializePreset(string presetFile);        
+    }
+}
