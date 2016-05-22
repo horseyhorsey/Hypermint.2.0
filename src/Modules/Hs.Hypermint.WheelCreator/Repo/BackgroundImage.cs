6106525e5d202be919b00deb1bd83bbeae7ba806
dave <dub.cyclone@gmail.com>
2016-05-22 23:06:27 +0100

    + BackgroundImage & Services

---
+++ src/Modules/Hs.Hypermint.WheelCreator/Repo/BackgroundImage.cs
@@ -0,0 +1,46 @@
+﻿using Hs.Hypermint.WheelCreator.Services;
+using ImageMagick;
+
+namespace Hs.Hypermint.WheelCreator.Repo
+{
+    public class BackgroundImage : IBackgroundImageService
+    {
+        public MagickImage FilledBackground(MagickColor bgColor, int width, int height)
+        {
+            var image = new MagickImage(bgColor, width, height);
+
+            return image;
+        }
+
+        public MagickImage PlasmaBackground(string color1 = "steelblue", string color2 = "black",
+        int width = 1024, int height = 768)
+        {
+            string plasmaImageText = "plasma:";
+            plasmaImageText += color1 + "-";
+            plasmaImageText += color2;
+
+            var plasmaImage = new MagickImage(plasmaImageText, width, height);
+
+            plasmaImage.Alpha(AlphaOption.Set);
+
+            plasmaImage.BackgroundColor = MagickColors.Transparent;
+
+            //plasmaImage.Vignette(0.5, 100, width, height);           
+
+            return plasmaImage;
+
+        }
+
+        public MagickImage FeatherImage(MagickImage image, MorphologyMethod method, Kernel kernel)
+        {
+            var feathered = new MagickImage(image);            
+
+            feathered.VirtualPixelMethod = VirtualPixelMethod.Transparent;
+
+            feathered.Morphology(method, kernel);
+
+            return feathered;
+        }
+
+    }
+}
---
+++ src/Modules/Hs.Hypermint.WheelCreator/Services/IBackgroundImageService.cs
@@ -0,0 +1,19 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hs.Hypermint.WheelCreator.Services
+{
+    public interface IBackgroundImageService
+    {
+        MagickImage FilledBackground(MagickColor bgColor, int width, int height);
+
+        MagickImage PlasmaBackground(string color1 = "steelblue", string color2 = "black",
+        int width = 1024, int height = 768);
+
+        MagickImage FeatherImage(MagickImage image, MorphologyMethod method, Kernel kernel);
+    }
+}
