4b63abf5e439467c0b93f9efb24a0186a57e2153
dave <dub.cyclone@gmail.com>
2016-05-22 23:11:07 +0100

    + add view model

---
+++ src/Modules/Hs.Hypermint.WheelCreator/Views/WheelProcessView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hs.Hypermint.WheelCreator.Views
+{
+    /// <summary>
+    /// Interaction logic for WheelProcessView
+    /// </summary>
+    public partial class WheelProcessView : UserControl
+    {
+        public WheelProcessView()
+        {
+            InitializeComponent();
+        }
+    }
+}
