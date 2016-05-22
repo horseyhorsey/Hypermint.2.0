1664075cef67c79ba89c00f4629537f52414f139
dave <dub.cyclone@gmail.com>
2016-05-22 23:02:36 +0100

    Update tests

--- src/Modules/Hs.Hypermint.WheelCreatorTests/Tools/WheelGenTests.cs
+++ src/Modules/Hs.Hypermint.WheelCreatorTests/Tools/WheelGenTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using ImageMagick;
 using System;
+using Hs.Hypermint.WheelCreator.Services;
+using Hs.Hypermint.WheelCreator.Repo;
 
 namespace Hs.Hypermint.WheelCreator.Tools.Tests
 {
@@ -8,25 +10,80 @@ namespace Hs.Hypermint.WheelCreator.Tools.Tests
     public class WheelGenTests
     {
         [Test()]
+        public void GenerateBackgroundColor()
+        {
+            IBackgroundImageService srvc = new BackgroundImage();
+
+            using (var bg = srvc.FilledBackground(MagickColors.Aqua, 400, 200))
+            {
+                bg.Write(@"c:\users\admin\desktop\magickBG.png");
+            }
+        }
+
+        [Test()]
+        public async void GenerateCaption()
+        {
+
+        }
+
+        [Test()]
+        public void GeneratePlasmaBackground()
+        {
+            IBackgroundImageService srvc = new BackgroundImage();
+
+            using (var bg = srvc.PlasmaBackground())
+            {
+                bg.Write(@"c:\users\admin\desktop\magickPlasmaBg.png");
+            }
+        }
+
+        [Test()]
+        public void GeneratePlasmaBackgroundFeathered()
+        {
+            IBackgroundImageService srvc = new BackgroundImage();
+
+            using (var bg = srvc.PlasmaBackground())
+            {
+                using (var feathered = srvc.FeatherImage(bg, MorphologyMethod.Erode, Kernel.Octagonal))
+                {
+                    feathered.Write(@"c:\users\admin\desktop\magickPlasmaBg.png");
+                }
+
+            }
+        }
+
+        [Test()]
+        public void TiledText()
+        {
+            using (var bg = new MagickImage("radial-gradient:green-yellow"))
+            {
+                using (var neh = WheelGen.CreateText(bg))
+                {
+                    neh.Write(@"c:\users\admin\desktop\magickPlasmaBg.png");
+                }
+            }
+        }
+
+        [Test()]
         public void GenerateLogoTest()
         {
             //TextOverlay();
 
             //Label();
 
-            // ShrinkFit();
+            ShrinkFit();
 
-            ImageMagickAnnotate();
+            //ImageMagickAnnotate();
         }
 
         private void ImageMagickAnnotate()
         {
-            using (MagickImage image = new MagickImage(MagickColors.Transparent,400,180))
+            using (MagickImage image = new MagickImage(MagickColors.Transparent, 400, 180))
             {
                 image.Settings.Font = "Candice";
                 image.Settings.FontPointsize = 100;
                 image.Settings.FillPattern = new MagickImage("pattern:checkerboard");
-                image.Annotate("Anthony\nis\ncheap", new MagickGeometry(400, 180));
+                image.Annotate("Anthony\nis\ncheap", new MagickGeometry(800, 500));
                 image.Write(@"c:\users\admin\desktop\font_tile.png");
             }
         }
@@ -41,9 +98,9 @@ namespace Hs.Hypermint.WheelCreator.Tools.Tests
                 MagickReadSettings settings = new MagickReadSettings();
                 settings.FrameIndex = 0; // First page
                 settings.FrameCount = 2; // Number of pages
-                // Read only the first page of the pdf file
-                
-                collection.Read(@"I:\RocketLauncher\Media\Manuals\Amstrad CPC\1st Division Manager (Europe)\Manual.pdf",settings);
+                                         // Read only the first page of the pdf file
+
+                collection.Read(@"I:\RocketLauncher\Media\Manuals\Amstrad CPC\1st Division Manager (Europe)\Manual.pdf", settings);
 
                 Console.WriteLine("PDF page count: {0}", collection.Count);
 
@@ -52,7 +109,7 @@ namespace Hs.Hypermint.WheelCreator.Tools.Tests
                 //Crop pages, for SuperNES
                 //appendedImage.Crop()
 
-                appendedImage.Write(@"c:\users\admin\desktop\mergedPdf.png");                
+                appendedImage.Write(@"c:\users\admin\desktop\mergedPdf.png");
             }
         }
 
@@ -71,48 +128,24 @@ namespace Hs.Hypermint.WheelCreator.Tools.Tests
 
         }
 
-        private static void Label()
+        [Test()]
+        public void Label()
         {
-
             using (MagickImage img = new MagickImage())
             {
-
                 img.Settings.FontStyle = FontStyleType.Italic;
                 img.Settings.FontWeight = FontWeight.Bold;
                 img.Settings.FillColor = new MagickColor("purple");
                 img.Settings.TextGravity = Gravity.Center;
-                img.Read("label:Magick.NET \nis chined", 0, 0);
+                img.Read("label:Magick.NET \nis chined", 400, 175);
                 img.Trim();
                 img.Write(@"c:\users\admin\desktop\font_tile.png");
             }
-
         }
 
         private static void ShrinkFit()
         {
-            string word = "Dizzy Ultimate cartoon Adventure";
 
-            using (MagickImage image = new MagickImage(MagickColors.Tomato, 400, 200))
-            {
-                image.Settings.FillColor = MagickColors.Purple;
-                image.Settings.Font = "Arial";
-                image.Settings.Density = new Density(72, 72);
-
-                image.Settings.FontPointsize = 36;
-                TypeMetric typeMetric = image.FontTypeMetrics(word);
-                while (typeMetric.TextWidth < image.Width)
-                {
-                    image.Settings.FontPointsize++;
-                    typeMetric = image.FontTypeMetrics(word);
-                }
-                image.Settings.FontPointsize--;
-
-                image.Settings.FillPattern = new MagickImage("pattern:checkerboard");
-
-                image.Annotate(word, Gravity.Center);
-
-                image.Write(@"c:\users\admin\desktop\font_tile.png");
-            }
         }
     }
 }
