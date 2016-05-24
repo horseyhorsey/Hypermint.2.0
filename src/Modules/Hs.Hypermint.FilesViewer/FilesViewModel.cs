5502a24f157562be481852779696209dbca9adf2
dave <dub.cyclone@gmail.com>
2016-05-25 00:57:29 +0100

    + Folder viewing & Refactor existing scan media methods

--- src/Modules/Hs.Hypermint.FilesViewer/FilesViewModel.cs
+++ src/Modules/Hs.Hypermint.FilesViewer/FilesViewModel.cs
@@ -9,12 +9,13 @@ using System.IO;
 using System.ComponentModel;
 using System.Windows.Data;
 using Prism.Commands;
+using System.Linq;
 
 namespace Hs.Hypermint.FilesViewer
 {
     public class FilesViewModel : ViewModelBase
     {
-        private string selectedGameName;
+        private string selectedGameName = "Files for:";
         public string SelectedGameName
         {
             get { return selectedGameName; }
@@ -28,12 +29,23 @@ namespace Hs.Hypermint.FilesViewer
             set { SetProperty(ref files, value); }
         }
 
+        private ICollectionView folders;
+        public ICollectionView Folders
+        {
+            get { return folders; }
+            set { SetProperty(ref folders, value); }
+        }
+
+        private string _systemName;
+        private string _mediaType;
+        private string _romName;
+
         private IEventAggregator _eventAggregator;
         private IAuditerRl _rlAuditer;
         private ISelectedService _selectedSrv;
         private ISettingsRepo _settingsRepo;
 
-        public FilesViewModel(IEventAggregator eventAggregator, IAuditerRl auditRl, 
+        public FilesViewModel(IEventAggregator eventAggregator, IAuditerRl auditRl,
             ISelectedService selectedSrv, ISettingsRepo settings)
         {
             _eventAggregator = eventAggregator;
@@ -46,33 +58,19 @@ namespace Hs.Hypermint.FilesViewer
 
         private void UpdateFiles(string[] HeaderAndGame)
         {
+            _systemName = _selectedSrv.CurrentSystem;
+            _romName = HeaderAndGame[1];
+            _mediaType = HeaderAndGame[0];
+
             try
             {
-                var mediaFiles = new List<MediaFile>();
-                Files = new ListCollectionView(mediaFiles);
+                UpdateMediaFiles();
+
+                UpdateRlFolders();
 
                 SelectedGameName = HeaderAndGame[0] + " Files for: " + HeaderAndGame[1];
 
-                var filesInFolder = _rlAuditer.GetFilesForMedia(_selectedSrv.CurrentSystem,
-                HeaderAndGame[1], _settingsRepo.HypermintSettings.RlMediaPath, HeaderAndGame[0]);
-
-                if (filesInFolder == null || filesInFolder.Length == 0)
-                {
-                    _eventAggregator.GetEvent<SetMediaFileRlEvent>().Publish("");
-                    return;
-                }
-
-                foreach (var item in filesInFolder)
-                {
-                    mediaFiles.Add(new MediaFile()
-                    { Name = Path.GetFileNameWithoutExtension(item), Extension = Path.GetExtension(item), FullPath = item });
-                }
-
-                Files = new ListCollectionView(mediaFiles);
-
                 ShowMediaPaneSelectedFile();
-
-                Files.CurrentChanged += Files_CurrentChanged;
 
             }
             catch (Exception ex)
@@ -80,6 +78,79 @@ namespace Hs.Hypermint.FilesViewer
                 _eventAggregator.GetEvent<ErrorMessageEvent>().Publish(ex.Message);
             }
 
+        }
+
+        private void UpdateMediaFiles(string addFolder = "")
+        {
+            var mediaFiles = new List<MediaFile>();
+            Files = new ListCollectionView(mediaFiles);
+
+            var filesInFolder =
+                _rlAuditer.GetFilesForMedia(
+                _systemName,
+                _romName,
+                _settingsRepo.HypermintSettings.RlMediaPath,
+                _mediaType,addFolder);
+
+            if (filesInFolder == null || filesInFolder.Length == 0)
+            {
+                _eventAggregator.GetEvent<SetMediaFileRlEvent>().Publish("");
+
+                return;
+            }
+
+            foreach (var item in filesInFolder)
+            {
+                mediaFiles.Add(new MediaFile()
+                { Name = Path.GetFileNameWithoutExtension(item), Extension = Path.GetExtension(item), FullPath = item });
+            }
+
+            Files = new ListCollectionView(mediaFiles);
+
+            Files.CurrentChanged += Files_CurrentChanged;
+
+        }
+
+        private void UpdateRlFolders()
+        {
+            var folders = new List<MediaFolder>();
+
+            Folders = new ListCollectionView(folders);
+
+            var gameMediaRootPath = Path.Combine(
+                _settingsRepo.HypermintSettings.RlMediaPath,
+                _mediaType, _systemName, _romName);
+
+            if (!Directory.Exists(gameMediaRootPath)) return;
+
+            folders.Add(new MediaFolder
+            {
+                FullPath = gameMediaRootPath,
+                Name = "Root"
+            });
+
+            var foldersInFolder =
+                _rlAuditer.GetFoldersForMediaColumn(
+                _systemName,
+                _romName,
+                _settingsRepo.HypermintSettings.RlMediaPath,
+                _mediaType);
+            
+            if (foldersInFolder != null)
+            {
+                foreach (var item in foldersInFolder)
+                {
+                    folders.Add(new MediaFolder
+                    {
+                        FullPath = item,
+                        Name = Path.GetFileName(item)
+                    });
+                }
+            }
+
+            Folders = new ListCollectionView(folders);
+
+            Folders.CurrentChanged += Folders_CurrentChanged;
 
         }
 
@@ -95,7 +166,7 @@ namespace Hs.Hypermint.FilesViewer
             {
                 _eventAggregator.GetEvent<ErrorMessageEvent>().Publish(ex.Message);
             }
-            
+
         }
 
         private void Files_CurrentChanged(object sender, EventArgs e)
@@ -103,6 +174,17 @@ namespace Hs.Hypermint.FilesViewer
             ShowMediaPaneSelectedFile();
         }
 
+        private void Folders_CurrentChanged(object sender, EventArgs e)
+        {
+            var folder = Folders.CurrentItem as MediaFolder;
+
+            if (folder.Name == "Root")
+                UpdateMediaFiles();
+            else
+                UpdateMediaFiles(folder.Name);
+
+            ShowMediaPaneSelectedFile();
+        }
 
         public class MediaFile
         {
@@ -113,6 +195,17 @@ namespace Hs.Hypermint.FilesViewer
             public override string ToString()
             {
                 return Name + Extension;
+            }
+        }
+
+        public class MediaFolder
+        {
+            public string Name { get; set; }
+            public string FullPath { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
             }
         }
     }
