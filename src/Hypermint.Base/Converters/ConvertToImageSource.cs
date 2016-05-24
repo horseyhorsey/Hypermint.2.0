f32fed4ec1b5d460540a055ca959e0e26132c5a0
dave <dub.cyclone@gmail.com>
2016-05-25 00:48:23 +0100

    + convertimages from base module

---
+++ src/Hypermint.Base/Converters/ConvertToImageSource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Hypermint.Base.Converters
+{
+    public class ConvertToImageSource
+    {
+        /// <summary>
+        /// Get imagesource from URI file link
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static ImageSource ImageSourceFromUri(Uri source)
+        {
+            try
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = source;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                bitmap.EndInit();
+
+                return bitmap;
+
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
