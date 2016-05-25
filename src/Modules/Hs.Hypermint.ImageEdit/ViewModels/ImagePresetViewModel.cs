8e3424af44b8c930c284021b3764a46f771bb58a
dave <dub.cyclone@gmail.com>
2016-05-25 02:46:52 +0100

    + save preset command

--- src/Modules/Hs.Hypermint.ImageEdit/ViewModels/ImagePresetViewModel.cs
+++ src/Modules/Hs.Hypermint.ImageEdit/ViewModels/ImagePresetViewModel.cs
@@ -1,12 +1,21 @@
-﻿using Hypermint.Base.Base;
+﻿using System;
+using Hypermint.Base.Base;
+using Prism.Commands;
 
 namespace Hs.Hypermint.ImageEdit.ViewModels
 {
     public class ImagePresetViewModel : ViewModelBase
     {
+        public DelegateCommand SavePresetCommand { get; private set; } 
+
         public ImagePresetViewModel()
         {
+            SavePresetCommand = new DelegateCommand(SavePreset);
+        }
 
+        private void SavePreset()
+        {
+            throw new NotImplementedException();
         }
     }
 }
