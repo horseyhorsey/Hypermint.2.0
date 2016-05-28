1c3ba6f905b22a1c90ddd7580d3f3cf3ca935d4d
dave <dub.cyclone@gmail.com>
2016-05-28 19:24:07 +0100

    Add ununsedfiles list view

---
+++ src/Modules/Hs.Hypermint.HyperspinFile/Controls/UnusedFilesList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hs.Hypermint.HyperspinFile.Controls
+{
+    /// <summary>
+    /// Interaction logic for FilesList.xaml
+    /// </summary>
+    public partial class UnusedFilesList : UserControl
+    {
+        public UnusedFilesList()
+        {
+            InitializeComponent();
+        }
+    }
+}
