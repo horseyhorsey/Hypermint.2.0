962f37ae1717b322a14bd10ae17f6856bf2bd70a
dave <dub.cyclone@gmail.com>
2016-05-29 03:31:37 +0100

    add yt searching views & model

--- src/Modules/Hs.Hypermint.Audits/ViewModels/YoutubeViewModel.cs
+++ src/Modules/Hs.Hypermint.Audits/ViewModels/YoutubeViewModel.cs
@@ -1,25 +1,33 @@
-﻿using Hypermint.Base.Base;
+﻿using Hypermint.Base;
+using Hypermint.Base.Base;
 using Hypermint.Base.Events;
 using Hypermint.Base.Interfaces;
 using Hypermint.Base.Services;
+using Prism.Commands;
 using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Runtime.CompilerServices;
 
 namespace Hs.Hypermint.Audits.ViewModels
 {
     public class YoutubeViewModel : ViewModelBase
     {
+        #region Services
         private ICollectionView videoList;
         private ISearchYoutube _youtube;
         private IEventAggregator _evtAggr;
         private ISelectedService _selectedService;
+        #endregion
 
+        #region Properties
         private Uri video;
         public Uri Video
         {
@@ -33,6 +41,82 @@ namespace Hs.Hypermint.Audits.ViewModels
             set { SetProperty(ref videoList, value); }
         }
 
+        private bool isSearching;
+        public bool IsSearching
+        {
+            get { return isSearching; }
+            set { SetProperty(ref isSearching, value); }
+        }
+
+        private bool videosVisible;
+        public bool VideosVisible
+        {
+            get { return videosVisible; }
+            set { SetProperty(ref videosVisible, value); }
+        }
+
+        private string youtubeViewHeader = "YT Search: ";
+        public string YoutubeViewHeader
+        {
+            get { return youtubeViewHeader; }
+            set { SetProperty(ref youtubeViewHeader, value); }
+        }
+
+        private bool includeSystem = true;
+        public bool IncludeSystem
+        {
+            get { return includeSystem; }
+            set { SetProperty(ref includeSystem, value); }
+        }
+
+        private bool includeDescription = true;
+        public bool IncludeDescription
+        {
+            get { return includeDescription; }
+            set
+            {
+                SetProperty(ref includeDescription, value);
+            }
+        }
+
+        private bool includeRomname = false;
+        public bool IncludeRomname
+        {
+            get { return includeRomname; }
+            set { SetProperty(ref includeRomname, value); }
+        }
+
+        private string searchTermText;
+        public string SearchTermText
+        {
+            get { return searchTermText; }
+            set { SetProperty(ref searchTermText, value); }
+        }
+
+        private CancellationTokenSource cancelSource;
+        #endregion
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            switch (propertyName)
+            {
+                case "IncludeDescription":
+                case "IncludeRomname":
+                case "IncludeSystem":
+                    BuildSearchTerm();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public DelegateCommand OpenYtCommand { get; private set; }
+        public DelegateCommand AuditViewCommand { get; private set; }
+        public DelegateCommand SearchYtCommand { get; private set; }
+        public DelegateCommand CancelSearchCommand { get; private set; } 
+
         public YoutubeViewModel(ISearchYoutube youtube, 
             IEventAggregator ea, ISelectedService selectedService)
         {
@@ -40,36 +124,120 @@ namespace Hs.Hypermint.Audits.ViewModels
             _evtAggr = ea;
             _selectedService = selectedService;
 
-            _evtAggr.GetEvent<GetVideosEvent>().Subscribe(BuildVideoList);
-            
-            //var tempList = new List<string>();
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    tempList.Add("chine" + i);
-            //}
+            _evtAggr.GetEvent<GetVideosEvent>().Subscribe(async (x) =>
+            {
 
-            //VideoList = new ListCollectionView(tempList);
+                IsSearching = true;
+                VideosVisible = false;
+
+                SearchTermText = BuildSearchTerm();
+
+                await BuildVideoList(x);
+
+            });
+
+            OpenYtCommand = new DelegateCommand(() =>
+            {
+                var vid = VideoList.CurrentItem as YoutubeVideo;
+
+                if (vid != null)
+                {
+                    try
+                    {
+                        Process.Start(vid.VideoUrl);
+                    }
+                    catch (Exception) { }
+                    
+                }
+
+            });
+
+            AuditViewCommand = new DelegateCommand(() =>
+            {
+                _evtAggr.GetEvent<NavigateRequestEvent>().Publish("HsMediaAuditView");
+            });
+
+            SearchYtCommand = new DelegateCommand(async () =>
+            {
+                IsSearching = true;
+                VideosVisible = false;
+
+                  await BuildVideoList(new object());
+
+            });
+
+            CancelSearchCommand = new DelegateCommand(() => {
+
+                cancelSource.Cancel();
+            });
         }
 
-        private void BuildVideoList(object x)
+        private string BuildSearchTerm()
         {
-            Task.Run(async () =>
-            {
-                var searchTerm = _selectedService.CurrentSystem + "+" + _selectedService.CurrentRomname;
+            var searchTerm = "";
+            var system = _selectedService.CurrentSystem;
+            var rom = _selectedService.CurrentRomname;
 
-                var links = await _youtube.SearchAsync(searchTerm);                
+            if (IncludeSystem)
+            {
+                if (isMainMenu(system))
+                    searchTerm = rom + " ";
+                else
+                    searchTerm = system + " ";
+            }
+
+            if (IncludeRomname)
+                if (!isMainMenu(system))
+                    searchTerm += rom + " ";
+
+            if (IncludeDescription)
+                if (!isMainMenu(system))
+                    searchTerm += _selectedService.CurrentDescription + " ";
+
+            SearchTermText = searchTerm;            
+
+            return searchTerm.TrimEnd(' ').Replace(' ', '+');
+        }
+
+        private bool isMainMenu(string system) => system.ToLower().Contains("main menu");
+
+        private async Task BuildVideoList(object x)
+        {
+            cancelSource = new CancellationTokenSource();
+
+            YoutubeViewHeader = "YT Search: " + SearchTermText;
+
+            await Task.Run(async () =>
+            {                
+                var links = await _youtube.SearchAsync(SearchTermText);
 
                 var ytVids = new List<YoutubeVideo>();
 
-                for (int i = 0; i < links.Count; i++)
+                try
                 {
-                    ytVids.Add(new YoutubeVideo(links[i]));                    
+                    for (int i = 0; i < links.Count; i++)
+                    {
+                        if (!cancelSource.IsCancellationRequested)
+                            ytVids.Add(new YoutubeVideo(links[i]));
+                        else
+                            break;
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    
+                }
+                finally
+                {
+                    IsSearching = false;
+                    VideosVisible = true;
 
+                    if (!cancelSource.IsCancellationRequested)
+                        VideoList = new ListCollectionView(ytVids);
                 }
 
-                VideoList = new ListCollectionView(ytVids);
-
-            });
+            }, cancelSource.Token);
 
         }
     }
@@ -89,6 +257,8 @@ namespace Hs.Hypermint.Audits.ViewModels
             SetVideoIdWatchUrl();
 
             SetThumbUrl();
+
+            SetVideoTitle();
         }
 
         public void SetVideoIdWatchUrl()
@@ -115,6 +285,11 @@ namespace Hs.Hypermint.Audits.ViewModels
                     VideoThumb = stdRes;
             }
 
+        }
+
+        public void SetVideoTitle()
+        {
+            
         }
 
 //        var client = new WebClient();
