47c2857e9b2b0a89e3a515e0210b5bd54f4e76aa
dave <dub.cyclone@gmail.com>
2016-05-29 08:06:56 +0100

    Fix rom mapping not overwriting.

--- src/Modules/Hs.Hypermint.MultiSystem/Services/RocketlaunchRomMap.cs
+++ src/Modules/Hs.Hypermint.MultiSystem/Services/RocketlaunchRomMap.cs
@@ -14,24 +14,19 @@ namespace Hs.Hypermint.MultiSystem.Services
             if (File.Exists(fi.FullName))
                 fi.Attributes &= ~FileAttributes.ReadOnly;
 
-            using (StreamWriter file = new StreamWriter(gamesIniPath + "\\games.ini", true))
-            {
+            using (StreamWriter file = new StreamWriter(gamesIniPath + "\\games.ini", false))
+            {                
                 file.WriteLine("# This file is only used for remapping specific games to other Emulators and/or Systems.");
                 file.WriteLine("# If you don't want your game to use the Default_Emulator, you would set the Emulator key here.");
                 file.WriteLine("# This file can also be used when you have Wheels with games from other Systems.");
                 file.WriteLine("# You would then use the System key to tell HyperLaunch what System to find the emulator settings.");
                 file.WriteLine("");
-            }
-
-            foreach (var game in gamesList)
-            {
-                using (StreamWriter file = new StreamWriter(gamesIniPath + "\\games.ini", true))
+                foreach (var game in gamesList)
                 {
                     file.WriteLine("[{0}]", game.RomName);
                     file.WriteLine(@"System={0}", game.System);
                 }
             }
-
         }
 
     }
