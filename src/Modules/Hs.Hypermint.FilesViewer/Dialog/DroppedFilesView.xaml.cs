d88029bdf075b683aabecfb256b68935f7945d0e
dave <dub.cyclone@gmail.com>
2016-05-30 20:01:46 +0100

    + dropped files dialog

---
+++ src/Modules/Hs.Hypermint.FilesViewer/Dialog/DroppedFilesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hs.Hypermint.FilesViewer.Dialog
+{
+    /// <summary>
+    /// Interaction logic for SaveDatabaseDialog.xaml
+    /// </summary>
+    public partial class DroppedFilesView : UserControl
+    {
+        public DroppedFilesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
