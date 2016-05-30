3e575bfab96367bc72a9c342417ea25e898d6814
dave <dub.cyclone@gmail.com>
2016-05-30 20:10:10 +0100

    Refactor

--- src/Modules/Hs.Hypermint.FilesViewer/FilesView.xaml.cs
+++ src/Modules/Hs.Hypermint.FilesViewer/FilesView.xaml.cs
@@ -1,41 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace Hs.Hypermint.FilesViewer
 {
     /// <summary>
     /// Interaction logic for FilesView.xaml
     /// </summary>
-    public partial class FilesView : UserControl
-    {
+    public partial class FilesView : UserControl { 
+
         public FilesView()
-        {
+        {            
             InitializeComponent();
         }
 
-        private void root_DragEnter(object sender, DragEventArgs e)
-        {        
-            if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop))
-                e.Effects = System.Windows.DragDropEffects.All;
-            else
-                e.Effects = System.Windows.DragDropEffects.None;
-        }    
-
-        private void root_Drop(object sender, DragEventArgs e)
-        {
-
-        }
     }
 }
