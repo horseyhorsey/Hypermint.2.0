14629e2c76e4a03719280319a55925506435b778
dave <dub.cyclone@gmail.com>
2016-05-30 20:02:09 +0100

    + name property to mediafile class

--- src/Modules/Hs.Hypermint.HyperspinFile/Models/UnusedMediaFile.cs
+++ src/Modules/Hs.Hypermint.HyperspinFile/Models/UnusedMediaFile.cs
@@ -3,6 +3,8 @@ namespace Hs.Hypermint.HyperspinFile.Models
 {
     public class MediaFile
     {
+        public string Name { get; set; }
+
         public string FileName { get; set; }
 
         public string Extension { get; set; }
