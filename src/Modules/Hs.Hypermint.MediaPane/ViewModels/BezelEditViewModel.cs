d87191328bf9c5c065ace1555e3d34db9ce66b03
dave <dub.cyclone@gmail.com>
2016-05-31 20:06:09 +0100

    + bezel viewmodel

---
+++ src/Modules/Hs.Hypermint.MediaPane/ViewModels/BezelEditViewModel.cs
@@ -0,0 +1,108 @@
+﻿using Hs.Hypermint.Services.Helpers;
+using Hypermint.Base;
+using Hypermint.Base.Base;
+using Prism.Commands;
+using Prism.Events;
+using System;
+
+namespace Hs.Hypermint.MediaPane.ViewModels
+{
+    public class BezelEditViewModel : ViewModelBase
+    {
+        public DelegateCommand PreviewMouseDownCommand { get; private set; }
+        public DelegateCommand SaveBezelIniCommand { get; private set; }
+
+        #region Properties
+        private string bezelHeader;
+        public string BezelHeader
+        {
+            get { return bezelHeader; }
+            set { SetProperty(ref bezelHeader, value); }
+        }
+
+        public string BezelImage
+        {
+            get { return bezelImage; }
+            set { SetProperty(ref bezelImage, value); }
+        }
+
+        private double leftClickX = 0;
+        public double LeftClickX
+        {
+            get { return leftClickX; }
+            set { SetProperty(ref leftClickX, value); }
+        }
+
+        private double leftClickY = 0;
+        public double LeftClickY
+        {
+            get { return leftClickY; }
+            set { SetProperty(ref leftClickY, value); }
+        }
+
+        private double rightClickX = 0;
+        public double RightClickX
+        {
+            get { return rightClickX; }
+            set { SetProperty(ref rightClickX, value); }
+        }
+
+        private double rightClickY = 0;
+        public double RightClickY
+        {
+            get { return rightClickY; }
+            set { SetProperty(ref rightClickY, value); }
+        }
+        #endregion
+
+        private string bezelImage;
+
+        private IEventAggregator _eventAgg;
+
+        public BezelEditViewModel(IEventAggregator ea)
+        {
+            _eventAgg = ea;
+
+            _eventAgg.GetEvent<SetBezelImagesEvent>().Subscribe(imageFile =>
+            {
+                BezelImage = imageFile;
+                BezelHeader = BezelImage;
+
+                try
+                {
+                    var bezelValues =
+                        RlStaticMethods.LoadBezelIniValues(PngToIni(BezelImage));
+
+                    LeftClickX = Convert.ToDouble(bezelValues[0]);
+                    LeftClickY = Convert.ToDouble(bezelValues[1]);
+                    RightClickX = Convert.ToDouble(bezelValues[2]);
+                    RightClickY = Convert.ToDouble(bezelValues[3]);
+                }
+                catch (Exception)
+                {
+                    
+                }
+
+
+            });
+
+            PreviewMouseDownCommand = new DelegateCommand(() =>
+            {
+
+            });
+
+            SaveBezelIniCommand = new DelegateCommand(() => {
+                
+                RlStaticMethods.SaveBezelIni(
+                new double[]
+                {
+                    LeftClickX, LeftClickY, RightClickX, RightClickY
+                }, PngToIni(BezelImage));
+
+            });
+        }
+
+        private string PngToIni(string file) => BezelImage.Replace(".png", ".ini");
+
+    }
+}
---
+++ src/Modules/Hs.Hypermint.MediaPane/Views/BezelEditView.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Hs.Hypermint.MediaPane.Views
+{
+    /// <summary>
+    /// Interaction logic for BezelEditView
+    /// </summary>
+    public partial class BezelEditView : UserControl
+    {
+        public BezelEditView()
+        {
+            InitializeComponent();
+
+        }
+
+        private void bezelImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var img = bezelImage.Source;
+            
+            if (img == null) return;
+
+            Point pos = Mouse.GetPosition(bezelImage);
+
+            var mousePos = e.GetPosition(bezelImage);
+
+            var pos2 = ImgControlCoordsToPixelCoords(pos, bezelImage.ActualWidth, bezelImage.ActualHeight);
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                LeftClickX.Value = Math.Round(pos2.X);
+                LeftClickY.Value = Math.Round(pos2.Y);
+            }
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                RightClickX.Value = Math.Round(pos2.X);
+                RightClickY.Value = Math.Round(pos2.Y);
+            }
+        }        
+
+        Point ImgControlCoordsToPixelCoords(Point locInCtrl, double imgCtrlActualWidth, double imgCtrlActualHeight)
+        {
+            if (bezelImage.Stretch == Stretch.None)
+                return locInCtrl;
+
+            Size renderSize = new Size(imgCtrlActualWidth, imgCtrlActualHeight);
+            Size sourceSize = new Size()
+            { Width = ((BitmapSource)bezelImage.Source).PixelWidth,
+                Height = ((BitmapSource)bezelImage.Source).PixelHeight };
+
+            double xZoom = renderSize.Width / sourceSize.Width;
+            double yZoom = renderSize.Height / sourceSize.Height;
+
+            if (bezelImage.Stretch == Stretch.Fill)
+                return new Point(locInCtrl.X / xZoom, locInCtrl.Y / yZoom);
+
+            double zoom;
+            if (bezelImage.Stretch == Stretch.Uniform)
+                zoom = Math.Min(xZoom, yZoom);
+            else // (imageCtrl.Stretch == Stretch.UniformToFill)
+                zoom = Math.Max(xZoom, yZoom);
+
+            return new Point(locInCtrl.X / zoom, locInCtrl.Y / zoom);
+        }
+
+
+
+        public static class MouseUtilities
+        {
+            public static Point CorrectGetPosition(Visual relativeTo)
+            {
+                Win32Point w32Mouse = new Win32Point();
+                GetCursorPos(ref w32Mouse);
+                return relativeTo.PointFromScreen(new Point(w32Mouse.X, w32Mouse.Y));
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            internal struct Win32Point
+            {
+                public int X;
+                public int Y;
+            };
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool GetCursorPos(ref Win32Point pt);
+        }
+
+    }
+}
