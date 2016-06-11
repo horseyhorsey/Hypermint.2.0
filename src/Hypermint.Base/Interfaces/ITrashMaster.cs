947f449e47aa6e6b7270560237c574aa356e49fb
dave <dub.cyclone@gmail.com>
2016-06-11 10:31:01 +0100

    + trashMaster service, interface

---
+++ src/Hypermint.Base/Interfaces/ITrashMaster.cs
@@ -0,0 +1,11 @@
+﻿namespace Hypermint.Base.Interfaces
+{
+    public interface ITrashMaster
+    {
+        void RlFileToTrash(string fileName, string system, string mediaType, string romName);
+
+        void HsFileToTrash(string fileName, string system, string mediaType, string romName);
+
+        string GetHsTrashPath(string system, string mediaType);
+    }
+}
