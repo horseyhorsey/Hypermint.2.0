c25e2c6f1aca6088de9b0eb219c523f40b10e4e7
dave <dub.cyclone@gmail.com>
2016-06-11 10:40:16 +0100

    + options placeholder for deeper options

---
+++ src/Modules/Hs.Hypermint.Audits/Views/RlAuditOptionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hs.Hypermint.Audits.Views
+{
+    /// <summary>
+    /// Interaction logic for RlAuditOptionView
+    /// </summary>
+    public partial class RlAuditOptionView : UserControl
+    {
+        public RlAuditOptionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
