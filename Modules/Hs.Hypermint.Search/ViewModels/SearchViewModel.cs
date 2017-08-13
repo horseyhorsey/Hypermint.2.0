4d0742fbef0b4acc5fc3c1ee5c7e6898bfd91812
dave <dub.cyclone@gmail.com>
2017-08-13 17:48:10 +0100

    Big clean, refactor organize solution. Move projects assemblys and tests to make more sense. Fix system filter bug if no systems were populated. Fix main menu search if rocketlaunch or hyperspin wasn't set.

---
+++ Modules/Hs.Hypermint.Search/ViewModels/SearchViewModel.cs
@@ -0,0 +1,376 @@
+﻿using Hs.HyperSpin.Database;
+using Hypermint.Base.Base;
+using Hypermint.Base.Interfaces;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Hypermint.Base.Services;
+using Prism.Events;
+using Hypermint.Base;
+using System.IO;
+using System.Threading;
+using Hs.Hypermint.DatabaseDetails.Services;
+using Hypermint.Base.Constants;
+using System.Collections;
+
+namespace Hs.Hypermint.Search.ViewModels
+{
+    public class SearchViewModel : HyperMintModelBase
+    {
+        #region Constructors
+        public SearchViewModel(IMainMenuRepo mainMenu,
+            IEventAggregator eventAggregator,
+            ISettingsRepo settings,
+            IHyperspinXmlService xmlService,
+            ISelectedService selectedSrv,
+            IGameLaunch gameLaunch): base(eventAggregator,selectedSrv,gameLaunch,settings)
+        {
+            _mainmenuRepo = mainMenu;
+            _xmlService = xmlService;            
+
+            //Search for games with the SearchString from this Vm.
+            SearchGamesCommand = new DelegateCommand<string>(async x =>
+            {                
+                x = SearchString;
+
+                if (x?.Length > 3)
+                {
+                    await ScanForGamesAsync(x);
+                }
+            }, o => canSearch);
+
+            CancelCommand = new DelegateCommand(() =>
+            {
+                tokenSource.Cancel();
+                canSearch = true;
+                SearchGamesCommand.RaiseCanExecuteChanged();
+            });
+
+            PageGamesCommand = new DelegateCommand<string>(x => PageGames(x));
+
+            SelectSystemsCommand = new DelegateCommand<string>(x =>
+            {
+                try
+                {
+                    var sysEnabled = 0;
+
+                    if (x == "all")
+                        sysEnabled = 1;
+
+                    foreach (MainMenu item in _systems)
+                    {
+                        item.Enabled = sysEnabled;
+                    }
+
+                    Systems.Refresh();
+
+                }
+                catch (Exception) { }
+
+            });
+
+            // Command for datagrid selectedItems
+            SelectionChanged = new DelegateCommand<IList>(
+                items =>
+                {
+                    if (items == null)
+                    {
+                        _selectedService.SelectedGames.Clear();
+                        return;
+                    }
+
+                    try
+                    {
+                        _selectedService.SelectedGames.Clear();
+                        foreach (var item in items)
+                        {
+                            var game = item as GameSearch;
+
+                            if (game.Game.RomName != null)
+                                _selectedService.SelectedGames.Add(game.Game);
+                        };
+                    }
+                    catch (Exception)
+                    {
+
+
+                    }
+
+                });            
+
+            DockSystemsCommand = new DelegateCommand(() => { SystemsVisible = !SystemsVisible; });
+
+            _eventAggregator.GetEvent<SystemsGenerated>().Subscribe(x => SystemsUpdated(x));
+
+        }
+        #endregion
+
+        #region Properties
+
+        private ICollectionView systems;
+        public ICollectionView Systems
+        {
+            get { return systems; }
+            set { SetProperty(ref systems, value); }
+        }
+        public int GamesFoundCount { get; set; }
+
+        private string pageInfo;
+        public string PageInfo
+        {
+            get { return pageInfo; }
+            set { SetProperty(ref pageInfo, value); }
+        }
+
+        private bool enabledSearchOn = true;
+        public bool EnabledSearchOn
+        {
+            get { return enabledSearchOn; }
+            set { SetProperty(ref enabledSearchOn, value); }
+        }
+
+        private bool cloneSearchOn = true;
+        public bool CloneSearchOn
+        {
+            get { return cloneSearchOn; }
+            set { SetProperty(ref cloneSearchOn, value); }
+        }
+
+        /// <summary>
+        /// Full collection view list
+        /// </summary>
+        private ICollectionView foundGmes;
+        public ICollectionView FoundGames
+        {
+            get { return foundGmes; }
+            set { SetProperty(ref foundGmes, value); }
+        }
+
+        private ICollectionView filteredGames;
+        public ICollectionView FilteredGames
+        {
+            get { return filteredGames; }
+            set { SetProperty(ref filteredGames, value); }
+        }
+
+        private List<GameSearch> searchedGames;
+
+        private string _searchString;
+        public string SearchString
+        {
+            get { return _searchString; }
+            set { SetProperty(ref _searchString, value); }
+        }
+
+        private bool systemsVisible = true;
+        public bool SystemsVisible
+        {
+            get { return systemsVisible; }
+            set { SetProperty(ref systemsVisible, value); }
+        }
+
+        #endregion
+
+        #region Fields
+        private bool canSearch = true;        
+        private IMainMenuRepo _mainmenuRepo;
+        private IHyperspinXmlService _xmlService;
+
+        private Systems _systems = new Systems();
+
+        /// <summary>
+        /// The current page count
+        /// </summary>
+        private int pageCount;
+
+        /// <summary>
+        /// The current search page
+        /// </summary>
+        private int currentPage;
+
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
+        #endregion
+
+        #region Commands
+        public DelegateCommand<string> SearchGamesCommand { get; }
+        public DelegateCommand<string> SelectSystemsCommand { get; }
+        public DelegateCommand CancelCommand { get; }
+        public DelegateCommand<string> PageGamesCommand { get; private set; }
+        public DelegateCommand<IList> SelectionChanged { get; set; }
+        public DelegateCommand<IList> ListBoxChanged { get; private set; }
+        public DelegateCommand DockSystemsCommand { get; private set; }     
+
+        #endregion
+
+        #region Support Methods        
+        private void SystemsUpdated(string system)
+        {
+
+            _systems = new Systems();
+
+            try
+            {
+                if (_mainmenuRepo.Systems != null)
+                {
+                    foreach (var item in _mainmenuRepo.Systems)
+                    {
+                        if (!item.Name.Contains("Main Menu"))
+                        {
+                            _systems.Add(new MainMenu()
+                            {
+                                Name = item.Name,
+                                SysIcon = item.SysIcon,
+                                Enabled = 1
+                            });
+                        }
+                    }
+
+                    Systems = new ListCollectionView(_systems);
+                }
+            }
+            catch (Exception) { }
+
+        }
+
+        /// <summary>
+        /// Method to scan from button inside the custom search dialog
+        /// </summary>
+        /// <returns></returns>
+        private async Task ScanForGamesAsync(string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm)) return;
+
+            tokenSource = new CancellationTokenSource();
+
+            canSearch = false;
+            SearchGamesCommand.RaiseCanExecuteChanged();
+
+            searchedGames = new List<GameSearch>();
+
+            await Task.Run(() =>
+            {
+                foreach (var system in _systems)
+                {
+                    try
+                    {
+                        if (system.Enabled == 1)
+                        {
+                            var games = _xmlService.SearchGames(
+                                _settingsRepo.HypermintSettings.HsPath,
+                                system.Name, searchTerm,CloneSearchOn,EnabledSearchOn);
+
+                            foreach (var game in games)
+                            {
+                                var mediaPath = Path.Combine(_settingsRepo.HypermintSettings.HsPath, Root.Media);
+
+                                game.GameEnabled = 1;
+
+                                var systemImage = Path.Combine(mediaPath, "Main Menu",
+                                    Images.Wheels, system.Name + ".png");
+
+                                var wheelImage = Path.Combine(mediaPath, system.Name,
+                                    Images.Wheels, game.RomName + ".png");
+
+                                searchedGames.Add(new GameSearch()
+                                {
+                                    Game = game,
+                                    SystemImage = systemImage,
+                                    WheelImage = wheelImage
+                                });
+                            }
+
+                        }
+                    }
+                    catch (Exception) { }
+                }
+
+                FoundGames = new ListCollectionView(searchedGames);
+                GamesFoundCount = searchedGames.Count;
+                currentPage = 1;
+
+                pageCount =  GamesFoundCount / 5 ;
+                if (GamesFoundCount % 5 != 0)
+                    pageCount++;
+
+                PageInfo = currentPage + " | " + pageCount + "      Games found: " + GamesFoundCount;
+
+                FilteredGames = new ListCollectionView(searchedGames.Take(5).ToList());
+
+                FilteredGames.CurrentChanged += FilteredGames_CurrentChanged;
+
+            }, tokenSource.Token);
+
+            canSearch = true;
+            SearchGamesCommand.RaiseCanExecuteChanged();
+
+        }
+        private void PageGames(string direction)
+        {
+            if (GamesFoundCount == 0) return;
+
+            if (direction == "forward")
+            {
+                if (currentPage < pageCount)
+                {
+                    FilteredGames = new ListCollectionView(
+                    searchedGames
+                    .Skip(5 * currentPage)
+                    .Take(5)
+                    .ToList());
+
+                    currentPage++;
+                }
+            }
+            else
+            {
+                if (currentPage > 1)
+                {
+                    currentPage--;
+
+                    if (currentPage == 1)
+                        FilteredGames =
+                            new ListCollectionView(
+                                searchedGames
+                                .Take(5).ToList());
+                    else
+                        FilteredGames = new ListCollectionView(
+                        searchedGames
+                        .Skip(5 * (currentPage - 1))
+                        .Take(5)
+                        .ToList());
+                }
+            }
+
+            FilteredGames.CurrentChanged += FilteredGames_CurrentChanged;
+
+            PageInfo = currentPage + " | " + pageCount + "  Games found: " + GamesFoundCount;
+        }
+        private void FilteredGames_CurrentChanged(object sender, EventArgs e)
+        {
+            _selectedService.SelectedGames.Clear();
+
+            var game = FilteredGames.CurrentItem as GameSearch;
+
+            if (game != null)
+                _selectedService.SelectedGames.Add(game.Game);
+
+        }
+        #endregion
+    }
+
+    #region Support classes
+    public class GameSearch
+    {
+        public Game Game { get; set; }
+        public string WheelImage { get; set; }
+
+        public string SystemImage { get; set; }
+    }
+
+    #endregion   
+}
+
---
+++ Modules/Hs.Hypermint.SidebarSystems/ViewModels/SystemsViewModel.cs
@@ -0,0 +1,419 @@
+﻿using Hs.HyperSpin.Database;
+using Hypermint.Base;
+using Hypermint.Base.Interfaces;
+using Prism.Events;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Data;
+using Hypermint.Base.Constants;
+using Hypermint.Base.Base;
+using Hypermint.Base.Services;
+using System.Windows.Media.Imaging;
+using System;
+using GongSolutions.Wpf.DragDrop;
+using System.Windows;
+using Prism.Commands;
+using Hypermint.Base.Events;
+using MahApps.Metro.Controls.Dialogs;
+using System.Threading.Tasks;
+using Hs.Hypermint.SidebarSystems.Dialog;
+using System.Reflection;
+
+namespace Hs.Hypermint.SidebarSystems.ViewModels
+{
+    public class SystemsViewModel : ViewModelBase, IDropTarget
+    {
+
+        #region Constructors
+        public SystemsViewModel()
+        {
+            _eventAggregator.GetEvent<AddNewSystemEvent>().Publish("SystemsView");
+        }
+
+        public SystemsViewModel(IMainMenuRepo main,
+            IEventAggregator eventAggregator,
+            ISettingsRepo settings,
+            IFileFolderService fileService,
+            IDialogCoordinator dialogService,
+            ISelectedService selectedService)
+        {
+            _mainMenuRepo = main;
+            _eventAggregator = eventAggregator;
+
+            _settingsRepo = settings;
+            _settingsRepo.LoadHypermintSettings();
+
+            _selectedService = selectedService;
+            _dialogService = dialogService;
+            _fileFolderService = fileService;
+
+            // Setup the main menu database to read in all systems
+            _mainMenuXmlPath = "";
+
+            _mainMenuXmlPath = Path.Combine(
+                    _settingsRepo.HypermintSettings.HsPath, Root.Databases,
+                    @"Main Menu\Main Menu.xml");
+
+            UpdateSystems(_mainMenuXmlPath);
+
+            _eventAggregator.GetEvent<MainMenuSelectedEvent>().Subscribe(UpdateSystems);
+            _eventAggregator.GetEvent<SystemFilteredEvent>().Subscribe(FilterSystemsByText);
+
+            SaveMainMenuCommand = new DelegateCommand(SaveMainMenu);
+
+            AddSystemCommand = new DelegateCommand<string>(async x =>
+            {
+                if (!Directory.Exists(_settingsRepo.HypermintSettings.HsPath)) return;
+
+                await AddSystemAsync();
+            });
+
+            CloseDialogCommand = new DelegateCommand(async () =>
+            {
+                await _dialogService.HideMetroDialogAsync(this, customDialog);
+            });
+
+            SelectDatabaseCommand = new DelegateCommand(() =>
+            {
+                if (!Directory.Exists(_settingsRepo.HypermintSettings.HsPath)) return;
+
+                PickedDatabaseXml =
+                    _fileFolderService.SetFileDialog(_settingsRepo.HypermintSettings.HsPath + "\\Databases");
+
+                ShortDbName = Path.GetFileNameWithoutExtension(PickedDatabaseXml);
+            });
+
+            SaveNewSystemCommand = new DelegateCommand(() =>
+            {
+                if (string.IsNullOrWhiteSpace(NewSystemName)) return;
+
+                bool createFromExisting = false;
+
+                if (!string.IsNullOrEmpty(PickedDatabaseXml))
+                    createFromExisting = true;
+
+                SaveNewSystem(createFromExisting);
+
+            });
+
+        }
+
+        #endregion
+
+        #region Properties
+        private ICollectionView _systemItems;
+        public ICollectionView SystemItems
+        {
+            get { return _systemItems; }
+            set { SetProperty(ref _systemItems, value); }
+        }
+
+        private bool systemListEnabled = true;
+        public bool SystemListEnabled
+        {
+            get { return systemListEnabled; }
+            set { SetProperty(ref systemListEnabled, value); }
+        }
+
+        private string _mainMenuXmlPath;
+
+        private bool reOrderSystems;
+        public bool ReOrderSystems
+        {
+            get { return reOrderSystems; }
+            set { SetProperty(ref reOrderSystems, value); }
+        }
+
+        private string systemsHeader = "Systems";
+        public string SystemsHeader
+        {
+            get { return systemsHeader; }
+            set { SetProperty(ref systemsHeader, value); }
+        }
+
+        private int systemCount;
+        public int SystemsCount
+        {
+            get { return systemCount; }
+            set { SetProperty(ref systemCount, value); }
+        }
+
+        private string pickedDatabaseXml;
+        public string PickedDatabaseXml
+        {
+            get { return pickedDatabaseXml; }
+            set { SetProperty(ref pickedDatabaseXml, value); }
+        }
+
+        private string shortDbName;
+        public string ShortDbName
+        {
+            get { return shortDbName; }
+            set { SetProperty(ref shortDbName, value); }
+        }
+
+        private string newSystemName;
+        public string NewSystemName
+        {
+            get { return newSystemName; }
+            set { SetProperty(ref newSystemName, value); }
+        }
+
+        #endregion
+
+        #region Services
+        IMainMenuRepo _mainMenuRepo;
+        ISettingsRepo _settingsRepo;
+        IEventAggregator _eventAggregator;
+        ISelectedService _selectedService;
+        IDialogCoordinator _dialogService;
+        CustomDialog customDialog;
+        private IFileFolderService _fileFolderService;
+        #endregion
+
+        #region Commands
+        public DelegateCommand SaveMainMenuCommand { get; private set; }
+        public DelegateCommand<string> AddSystemCommand { get; private set; }
+        public DelegateCommand CloseDialogCommand { get; private set; }
+        public DelegateCommand SelectDatabaseCommand { get; private set; }
+        public DelegateCommand SaveNewSystemCommand { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+        public void DragOver(IDropInfo dropInfo)
+        {
+            var sourceItem = dropInfo.Data as MainMenu;
+            var targetItem = dropInfo.TargetItem as MainMenu;
+
+            if (sourceItem != null && targetItem != null)
+            {
+                dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+                dropInfo.Effects = DragDropEffects.Copy;
+            }
+
+        }
+        public void Drop(IDropInfo dropInfo)
+        {
+            var sourceItem = dropInfo.Data as MainMenu;
+            var targetItem = dropInfo.TargetItem as MainMenu;
+
+            var AddInIndex = _mainMenuRepo.Systems.IndexOf(targetItem);
+
+            if (AddInIndex == 0)
+                AddInIndex = 1;
+
+            _mainMenuRepo.Systems.Remove(sourceItem);
+            _mainMenuRepo.Systems.Insert(AddInIndex, sourceItem);
+        }
+        #endregion
+
+        #region Support Methods
+
+        /// <summary>
+        /// Saves a new system to the main menu
+        /// </summary>
+        /// <param name="createFromExistingDb"></param>
+        private void SaveNewSystem(bool createFromExistingDb)
+        {
+            //Add system to the list
+            _mainMenuRepo.Systems.Add(new MainMenu()
+            {
+                Enabled = 1,
+                Name = NewSystemName
+            });
+
+            //Set the settings file for this system
+            var settingsFile = _settingsRepo.HypermintSettings.HsPath + "\\Settings\\" + NewSystemName + ".ini";
+
+            //Create settings file for system if not already existing.
+            if (!File.Exists(settingsFile))
+            {
+                //Load templated hyperspin settings ini from embedded resource
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "Hs.Hypermint.SidebarSystems.systemSettings.ini";
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (var reader = new StreamReader(stream))
+                using (var textFile = File.CreateText(settingsFile))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        textFile.WriteLine(line);
+                    }
+                }
+            }
+
+            CreateMediaDirectorysForNewSystem(_settingsRepo.HypermintSettings.HsPath, NewSystemName);
+
+            //Create Database file or directory
+            string dbDir = _settingsRepo.HypermintSettings.HsPath + "\\Databases\\";
+
+            if (!Directory.Exists(dbDir + NewSystemName))
+            {
+                Directory.CreateDirectory(dbDir + NewSystemName);
+            }
+
+            if (!createFromExistingDb) //Create a blank database with new system name.
+            {
+                File.Create(dbDir + NewSystemName + "\\" + NewSystemName + ".xml");
+            }
+            else // Create a new database from an existing hyperspin xml
+            {
+                if (!File.Exists(dbDir + NewSystemName + "\\" + NewSystemName + ".xml"))
+                {
+                    File.Copy(PickedDatabaseXml, dbDir + NewSystemName + "\\" + NewSystemName + ".xml");
+                }
+            }
+
+            _dialogService.HideMetroDialogAsync(this, customDialog);
+        }
+        /// <summary>
+        /// Generate directorys for a hyperspin system
+        /// </summary>
+        /// <param name="hsPath">Path to hyperspin</param>
+        /// <param name="systemName">The system name</param>
+        private void CreateMediaDirectorysForNewSystem(string hsPath, string systemName)
+        {
+            var newSystemMediaPath = Path.Combine(hsPath, Root.Media, systemName);
+
+            CreateDefaultHyperspinFolders(newSystemMediaPath);
+        }
+        /// <summary>
+        /// Creates all needed folders for a hyperspin system
+        /// </summary>
+        /// <param name="hyperSpinSystemMediaDirectory"></param>
+        private void CreateDefaultHyperspinFolders(string hyperSpinSystemMediaDirectory)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\Images\\Artwork" + i);
+            }
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Images.Backgrounds);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Images.GenreBackgrounds);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Images.GenreWheel);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Images.Letters);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Images.Pointer);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Images.Special);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Images.Wheels);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Root.Themes);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Sound.BackgroundMusic);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Sound.SystemExit);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Sound.SystemStart);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Sound.WheelSounds);
+            Directory.CreateDirectory(hyperSpinSystemMediaDirectory + "\\" + Root.Video);
+        }
+        private async Task AddSystemAsync()
+        {
+            customDialog = new CustomDialog() { Title = "Add new system" };
+
+            customDialog.Content = new AddSystemDialog { DataContext = this };
+
+            await _dialogService.ShowMetroDialogAsync(this, customDialog);
+        }
+        private void SaveMainMenu()
+        {
+            if (_selectedService.CurrentMainMenu == null) return;
+
+            try
+            {
+                _eventAggregator.GetEvent<SaveMainMenuEvent>()
+                    .Publish(_selectedService.CurrentMainMenu);
+            }
+            catch (Exception) { }
+
+        }
+        private void UpdateSystems(string mainMenuXml)
+        {
+            if (File.Exists(mainMenuXml))
+            {
+                _mainMenuXmlPath = mainMenuXml;
+                _mainMenuRepo.BuildMainMenuItems(mainMenuXml, _settingsRepo.HypermintSettings.RlMediaPath + @"\Icons\");
+                SystemItems = new ListCollectionView(_mainMenuRepo.Systems);
+
+                //Subscribe here again?? 
+                //##    Existing unsubscribes when the system list is changed.
+                SystemItems.CurrentChanged += SystemItems_CurrentChanged;
+
+                SystemsCount = _mainMenuRepo.Systems.Count - 1;
+                SystemsHeader = "Systems: " + SystemsCount;
+
+                _eventAggregator.GetEvent<SystemsGenerated>().Publish("");
+
+            }
+        }
+        /// <summary>
+        /// Filter the systems list
+        /// </summary>
+        /// <param name="obj"></param>
+        private void FilterSystemsByText(string filter)
+        {
+            if (SystemItems != null)
+            {
+                //Unsubscribe when the filter is being set
+                //Avoiding the systems databases loading on each filter change            
+                SystemItems.CurrentChanged -= SystemItems_CurrentChanged;
+
+                ICollectionView cv;
+
+                cv = CollectionViewSource.GetDefaultView(SystemItems);
+
+                cv.Filter = o =>
+                {
+                    var m = o as MainMenu;
+
+                    var textFiltered = m.Name.ToUpper().Contains(filter.ToUpper());
+                    return textFiltered;
+                };
+
+                SystemItems.CurrentChanged += SystemItems_CurrentChanged;
+
+            }            
+
+        }
+        private void SystemItems_CurrentChanged(object sender, System.EventArgs e)
+        {
+            MainMenu system = SystemItems.CurrentItem as MainMenu;
+
+            if (system != null)
+            {
+
+                _selectedService.CurrentSystem = system.Name;
+
+                if (!ReOrderSystems)
+                {
+                    _eventAggregator.GetEvent<PreviewGeneratedEvent>().Publish("");
+
+                    SetSystemImage();
+
+                    this._eventAggregator.GetEvent<SystemSelectedEvent>().Publish(system.Name);
+                }
+            }
+        }
+        /// <summary>
+        /// Set wheel image for the system
+        /// </summary>
+        /// <param name="path"></param>
+        private void SetSystemImage()
+        {
+            var imagePath = _settingsRepo.HypermintSettings.HsPath +
+                "\\Media\\Main Menu\\Images\\Wheel\\" +
+                _selectedService.CurrentSystem + ".png";
+
+            if (File.Exists(imagePath))
+                _selectedService.SystemImage = setImage(imagePath);
+            else
+                _selectedService.SystemImage = null;
+
+        }
+        private BitmapImage setImage(string imagePath)
+        {
+            Uri uriSource;
+            uriSource = new Uri(imagePath);
+            return new BitmapImage(uriSource);
+        }
+        #endregion
+
+    }
+}
