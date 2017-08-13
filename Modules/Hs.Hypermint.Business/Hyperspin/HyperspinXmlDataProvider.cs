b0852140c172d2d458ed8fd398bf97e05889bf9a
dave <dub.cyclone@gmail.com>
2017-08-13 21:13:39 +0100

    Add new dataprovider for hs.

---
+++ Modules/Hs.Hypermint.Business/Hyperspin/HyperspinXmlDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using Frontend.Helper.Model.Hyperspin;
+using Frontend.Helper.Serialization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hs.Hypermint.Business.Hyperspin
+{
+    public class HyperspinDataProvider : IHyperspinXmlDataProvider
+    {
+        private IHyperspinSerializer _hsSerializer;
+
+        public HyperspinDataProvider()
+        {
+        }
+
+        public async Task<IEnumerable<Game>> GetAllGames(string frontendPath, string systemName, string dbName = "")
+        {
+            _hsSerializer = new HyperspinSerializer(frontendPath, systemName, dbName);
+
+            return await _hsSerializer.DeserializeAsync();
+        }
+
+        public async Task<IEnumerable<MainMenu>> GetAllSystems(string frontendPath, string systemName, string dbName = "")
+        {
+            _hsSerializer = new HyperspinSerializer(frontendPath, systemName, dbName);
+
+            return await _hsSerializer.DeserializeMenusAsync();
+        }
+    }
+}
---
+++ Modules/Hs.Hypermint.Business/Hyperspin/IHyperspinDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using Frontend.Helper.Model.Hyperspin;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hs.Hypermint.Business.Hyperspin
+{
+    public interface IHyperspinXmlDataProvider
+    {
+        Task<IEnumerable<Game>> GetAllGames(string frontendPath, string systemName, string dbName = "");
+
+        Task<IEnumerable<MainMenu>> GetAllSystems(string frontendPath, string systemName, string dbName = "");
+    }
+}
