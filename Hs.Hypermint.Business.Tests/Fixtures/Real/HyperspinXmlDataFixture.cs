43d37f49fbaba6998d14431ff6d7c83df3d80d3c
dave <dub.cyclone@gmail.com>
2017-08-13 21:14:35 +0100

    Add fixture for hsXml

---
+++ Hs.Hypermint.Business.Tests/Fixtures/Real/HyperspinXmlDataFixture.cs
@@ -0,0 +1,18 @@
+﻿using Hs.Hypermint.Business.Hyperspin;
+using Xunit;
+
+namespace Hs.Hypermint.BusinessTests.Fixtures.Real
+{
+    [CollectionDefinition("HyperspinXmlDataCollection")]
+    public class DatabaseCollection : ICollectionFixture<HyperspinXmlDataFixture> { }
+
+    public class HyperspinXmlDataFixture
+    {
+        public IHyperspinXmlDataProvider _xmlDataProvider;
+
+        public HyperspinXmlDataFixture()
+        {
+            _xmlDataProvider = new HyperspinDataProvider();
+        }
+    }
+}
