043340ef6b96866b60dfa0cba5f4b17731082dad
dave <dub.cyclone@gmail.com>
2017-08-25 09:40:07 +0100

    change rlmedia folder scanner to take IEnum of games instead of old repo.

--- Modules/Hs.Hypermint.Services/RocketMediaFolderScanner.cs
+++ Modules/Hs.Hypermint.Services/RocketMediaFolderScanner.cs
@@ -1,5 +1,4 @@
 ﻿using Frontends.Models.Hyperspin;
-using Hypermint.Base.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -45,6 +44,51 @@ namespace Hs.Hypermint.Services
 
         #endregion
 
+        #region Public Methods
+
+        /// <summary>
+        /// Matches the rocketlaunch media folder to games.
+        /// </summary>
+        /// <param name="directories">The directories.</param>
+        /// <param name="gameRepo">The game repo.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NullReferenceException">No games exist in gameRepo</exception>
+        public RocketMediaFolderScanResult MatchFoldersToGames(string[] directories, IEnumerable<Game> gamesList)
+        {
+            if (gamesList.Count() == 0)
+                throw new NullReferenceException("No games exist in games list");
+
+            int matchedFolderCount = 0;
+            int[] results = new int[4];
+            RocketMediaFolderScanResult result = new RocketMediaFolderScanResult("");
+
+            //If a directory matches a game in the list , increment the matched count
+            foreach (var directory in directories)
+            {
+                //var dirName = Path.GetFileNameWithoutExtension(directory);
+                var dirName = Path.GetFileName(directory);
+
+                if (gamesList.Any(x => x.RomName.ToLower() == dirName.ToLower()))
+                {
+                    matchedFolderCount++;
+                    result.MatchedFolders.Add(dirName);
+                }
+                else
+                {
+                    result.UnMatchedFolders.Add(dirName);
+                }
+            }
+
+            results[0] = directories.Count();
+            results[1] = matchedFolderCount;
+            results[2] = gamesList.Count() - matchedFolderCount;
+            results[3] = directories.Count() - matchedFolderCount;
+
+            return result;
+        }
+
+        #endregion
+
         #region Support Methods        
 
         /// <summary>
@@ -59,71 +103,6 @@ namespace Hs.Hypermint.Services
             return Directory.GetDirectories(dir);
         }
 
-        /// <summary>
-        /// Matches the rocketlaunch media folder to games.
-        /// </summary>
-        /// <param name="directories">The directories.</param>
-        /// <param name="gameRepo">The game repo.</param>
-        /// <returns></returns>
-        /// <exception cref="System.NullReferenceException">No games exist in gameRepo</exception>
-        public RocketMediaFolderScanResult MatchFoldersToGames(string[] directories, IGameRepo gameRepo)
-        {
-            if (gameRepo != null && gameRepo.GamesList == null)
-                gameRepo.GamesList = new Games();
-
-            if (gameRepo?.GamesList.Count == 0)
-                throw new NullReferenceException("No games exist in gameRepo");
-
-            int matchedFolderCount = 0;
-            int[] results = new int[4];
-            RocketMediaFolderScanResult result = new RocketMediaFolderScanResult("");
-
-            //If a directory matches a game in the list , increment the matched count
-            foreach (var directory in directories)
-            {
-                //var dirName = Path.GetFileNameWithoutExtension(directory);
-                var dirName = Path.GetFileName(directory);
-
-                if (gameRepo.GamesList.Any(x => x.RomName.ToLower() == dirName.ToLower()))
-                {
-                    matchedFolderCount++;
-                    result.MatchedFolders.Add(dirName);
-                }
-                else
-                {
-                    result.UnMatchedFolders.Add(dirName);
-                }
-            }
-
-            results[0] = directories.Count();
-            results[1] = matchedFolderCount;
-            results[2] = gameRepo.GamesList.Count - matchedFolderCount;
-            results[3] = directories.Count() - matchedFolderCount;
-
-            return result;
-        }
-
-        #endregion
-
-        #region Public Methods
-        /// <summary>
-        /// Loads the games from a hyperspin XML. Loads games into list if xml exists.
-        /// </summary>
-        /// <param name="system">The system.</param>
-        /// <param name="gameRepo">The game repo.</param>
-        /// <returns></returns>
-        /// <exception cref="System.IO.FileNotFoundException"></exception>
-        public IGameRepo LoadGamesFromHyperspinXml(string system, IGameRepo gameRepo)
-        {
-            var hsDatabasePath = $"{HyperSpinPath}Databases\\{system}\\{system}.xml";
-
-            if (!File.Exists(hsDatabasePath))
-                throw new FileNotFoundException();
-
-            gameRepo?.GetGames(hsDatabasePath, $"{system}");
-
-            return gameRepo;
-        }
         #endregion
     }
 }
