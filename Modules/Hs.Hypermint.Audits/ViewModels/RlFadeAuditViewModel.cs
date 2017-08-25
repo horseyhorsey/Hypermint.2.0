cab2526ce92bcfdd799f8680d81284ec0fa64326
dave <dub.cyclone@gmail.com>
2017-08-25 07:28:29 +0100

    implement scanning fade from button

--- Modules/Hs.Hypermint.Audits/ViewModels/RlFadeAuditViewModel.cs
+++ Modules/Hs.Hypermint.Audits/ViewModels/RlFadeAuditViewModel.cs
@@ -4,22 +4,35 @@ using Hypermint.Base;
 using Hypermint.Base.Interfaces;
 using Hypermint.Base.Services;
 using Prism.Events;
+using System.Linq;
 
 namespace Hs.Hypermint.Audits.ViewModels
 {
     public class RlFadeAuditViewModel : RlAuditMediaViewModelBase
     {
+        private IRlScan _rlScan;
+        private ISettingsHypermint _settings;
+
         public RlFadeAuditViewModel()
         {
         }
 
-        public RlFadeAuditViewModel(IEventAggregator ea, IHyperspinManager hyperspinManager, IGameLaunch gameLaunch, ISettingsHypermint settings, ISelectedService selected, IRlScan rlScan) : base(ea, hyperspinManager, gameLaunch, settings, selected, rlScan)
+        public RlFadeAuditViewModel(IEventAggregator ea, IHyperspinManager hyperspinManager, IGameLaunch gameLaunch, ISettingsHypermint settings, ISelectedService selected,
+            IRlScan rlScan) : base(ea, hyperspinManager, gameLaunch, settings, selected, rlScan)
         {
+            _rlScan = rlScan;
+            _settings = settings;
         }
 
-        public override Task ScanForMedia()
+        public override async Task ScanForMedia()
         {
-            throw new NotImplementedException();
+            IsBusy = true;
+
+            if (_hyperspinManager.CurrentSystemsGames.Count > 0)
+                await _rlScan.ScanFadeAsync(_hyperspinManager.CurrentSystemsGames.Select(x => x.Game), 
+                    _settings.HypermintSettings.RlPath + "\\Media");
+
+            IsBusy = false;
         }
     }
 }
