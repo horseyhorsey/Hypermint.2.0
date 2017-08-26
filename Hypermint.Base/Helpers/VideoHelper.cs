79098968494361214f5593b8526fdfbeb6605f1d
dave <dub.cyclone@gmail.com>
2017-08-26 13:06:21 +0100

    Move videohelper to base.

--- Hypermint.Base/Helpers/VideoHelper.cs
+++ Hypermint.Base/Helpers/VideoHelper.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 
-namespace Hs.Hypermint.VideoEdit.Helpers
+namespace Hypermint.Base
 {
     public static class VideoHelper
     {
@@ -10,9 +10,27 @@ namespace Hs.Hypermint.VideoEdit.Helpers
         {
             var startStr = start.ToString(@"hh\:mm\:ss");
             var endStr = end.ToString(@"hh\:mm\:ss");
-
-            int i = 0;
             var startInfo = new ProcessStartInfo(ffmpeg + "\\ffmpeg.exe");
+            string outputNewFile = CreateIncrementalFileName(inputFile);
+
+            inputFile = "\"" + inputFile + "\"";
+            outputNewFile = "\"" + outputNewFile + "\"";
+
+
+            //startInfo.Arguments = $"-i {inputFile} -vcodec copy -acodec copy -ss {start.ToString()} -to {end.ToString()} {outputNewFile}";
+
+            //Transcode
+            startInfo.Arguments = $"-ss {start.ToString()} -i {inputFile} -vcodec copy -acodec copy -t {(end - start).ToString()} {outputNewFile}";
+
+            //ffmpeg - ss 00:08:00 - i Video.mp4 - ss 00:01:00 - t 00:01:00 - c copy VideoClip.mp4
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            Process.Start(startInfo).WaitForExit();
+        }
+
+        public static string CreateIncrementalFileName (string inputFile)
+        {
+            int i = 0;
             var fullPath = Path.GetDirectoryName(inputFile);
             var name = Path.GetFileName(inputFile);
             var ext = Path.GetExtension(name);
@@ -31,19 +49,7 @@ namespace Hs.Hypermint.VideoEdit.Helpers
                 outputNewFile = fullPath + "\\" + incrementout;
             }
 
-            inputFile = "\"" + inputFile + "\"";
-            outputNewFile = "\"" + outputNewFile + "\"";
-
-
-            //startInfo.Arguments = $"-i {inputFile} -vcodec copy -acodec copy -ss {start.ToString()} -to {end.ToString()} {outputNewFile}";
-
-            //Transcode
-            startInfo.Arguments = $"-ss {start.ToString()} -i {inputFile} -vcodec copy -acodec copy -t {(end - start).ToString()} {outputNewFile}";
-
-            //ffmpeg - ss 00:08:00 - i Video.mp4 - ss 00:01:00 - t 00:01:00 - c copy VideoClip.mp4
-            startInfo.UseShellExecute = false;
-            startInfo.CreateNoWindow = true;
-            Process.Start(startInfo).WaitForExit();
+            return outputNewFile;
         }
     }
 }
