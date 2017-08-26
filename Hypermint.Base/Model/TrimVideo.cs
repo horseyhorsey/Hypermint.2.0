61061cee1f6e37d4f35d93c47c2f020d0e2d4ab4
dave <dub.cyclone@gmail.com>
2017-08-26 08:46:48 +0100

    add model for passing around

---
+++ Hypermint.Base/Model/TrimVideo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hypermint.Base.Model
+{
+    public class TrimVideo
+    {
+        public string SystemName { get; set; }
+        public string RomName { get; set; }
+        public string File { get; set; }
+        public TimeSpan Start { get; set; }
+        public TimeSpan End { get; set; }
+    }
+}
