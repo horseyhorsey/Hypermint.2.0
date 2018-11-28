bb10b6ec4faeeeaa68f0b1aa7a09170a5bea4a68
dave <dub.cyclone@gmail.com>
2018-11-28 22:43:28 +0000

    Add TargetDir for installer.

--- src/Hypermint.Shell/Properties/SharedAssemblyInfo.cs
+++ src/Hypermint.Shell/Properties/SharedAssemblyInfo.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
 
-[assembly: AssemblyVersion("2.0.0.33")]
-[assembly: AssemblyFileVersion("2.0.0.33")]
+
+[assembly: AssemblyVersion("2.0.0.52")]
+[assembly: AssemblyFileVersion("2.0.0.52")]
