180b759f20f6fd82191b5610eae9e180eb0d2ba1
dave <dub.cyclone@gmail.com>
2018-11-28 22:30:45 +0000

    Add ignored files, no idea. Add installer to build config

---
+++ src/Hypermint.Base/Interfaces/ISearchYoutube.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hypermint.Base.Interfaces
+{
+    public interface ISearchYoutube
+    {
+        Task<List<string>> SearchAsync(string searchTerm);
+
+        /// <summary>
+        /// youtube-dl.exe
+        /// </summary>
+        /// <returns></returns>
+        Task YtDownload(string url, string outputPath, Action<string> outputCallback, CancellationToken token);
+    }
+}
